c7334a81afac235851a51afcdae9217d97de1b1f
João Lucas <155165579+JoaoLlucas2@users.noreply.github.com>
2025-03-15 17:41:27 -0300

    file renamed

--- src/main/java/org/classes/testes/BookTest.java
+++ src/main/java/org/classes/testes/BookTest.java
@@ -2,7 +2,7 @@ package org.classes.testes;
 
 import org.classes.Book;
 
-public class BookTeste {
+public class BookTest {
     public static void main(String[] args) {
         Book livro1 = new Book("Senhor dos Anéis", "Tolkien", "Fantasy");
         System.out.println(livro1);
