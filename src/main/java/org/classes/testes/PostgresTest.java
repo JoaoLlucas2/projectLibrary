768e6504c914d24714a0450711b10091b98883b1
João Lucas <155165579+JoaoLlucas2@users.noreply.github.com>
2025-03-24 16:39:54 -0300

    Classe de teste para conexão com Banco de Dados (PostgreSQL) - Em Aprendizado...

---
+++ src/main/java/org/classes/testes/PostgresTest.java
@@ -0,0 +1,35 @@
+package org.classes.testes;
+
+import java.io.InputStream;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class PostgresTest {
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+
+        try (InputStream fileIn = PostgresTest.class.getClassLoader().getResourceAsStream("config.properties")) {
+
+            if(fileIn == null) {
+                throw new IllegalArgumentException("Arquivo especificado não encontrado. ");
+            }
+
+            props.load(fileIn);
+            String url = props.getProperty("db.url");
+            String username = props.getProperty("db.username");
+            String password = props.getProperty("db.password");
+
+            try (Connection connect = DriverManager.getConnection(url, username, password)) {
+                System.out.println("Conexão com o Banco de Dados bem sucedida. ");
+            }catch (SQLException e) {
+                System.out.println("Erro ao conectar com o Banco de Dados: " + e.getMessage());
+            }
+
+        }catch (Exception e) {
+            System.out.println("Erro ao carregar o arquivo de configuração: " + e.getMessage());
+        }
+    }
+}
